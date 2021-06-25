c9ba0588b7991ee0f048944e63cb8eff05c3dcc9
Guillermo <you@example.com>
2021-06-25 12:51:24 +0200

    logOut actualizado

--- assets/js/script.js
+++ assets/js/script.js
@@ -17,30 +17,34 @@ $(document).ready(function () {
 });
 
 let logOut = document.getElementById('logOut');
-let alerta = document.getElementById('content')
+let alerta = document.getElementById('alerta')
 let confirmar = document.getElementById('confirmar')
 
 logOut.addEventListener('click',  () =>{
 
-    alerta.innerHTML += `<div class="modal fade" id="exampleModalCenter" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
-    <div class="modal-dialog modal-dialog-centered" role="document">
-      <div class="modal-content">
-        <div class="modal-header">
-          <h5 class="modal-title" id="exampleModalLongTitle">Confirmación de cierre de sesión</h5>
-          <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-            <span aria-hidden="true">&times;</span>
-          </button>
-        </div>
-        <div class="modal-body">
-          ¿Estás seguro de que quieres cerrar sesión?
-        </div>
-        <div class="modal-footer">
-          <button id="confirmar" type="button" class="btn btn-secondary" data-dismiss="modal">Sí, quiero salir.</button>
-          <button id="cancelar" type="button" class="btn btn-primary" data-dismiss="modal">No, quiero volver.</button>
-        </div>
-      </div>
-    </div>
-  </div>`;
 
+ 
     
+   
+   
 });
+
+confirmar.addEventListener('click', ()=>{
+    
+  // const logOutUser = JSON.parse(localStorage.getItem('token'));
+  // logOutUser.forEach((token, index) => {
+  //   if(token.token === token){
+  //   logOutUser.splice(index, 1)
+  //   }
+  // });
+  // localStorage.setItem('token',JSON.stringify(logOutUser));
+  alerta.innerHTML = `<div class="alert alert-success alert-dismissable">
+  <button type="button" class="close" data-dismiss="alert">&times;</button>
+  Has cerrado sesión con éxito, vuelve pronto!
+</div>`
+  setTimeout(
+    function(){
+        window.location.href = "../index.html";
+    }, 3000);
+
+}); 
