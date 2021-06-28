0bd8cfef7a960490d4bc44d91e355814895fba6f
Guillermo <you@example.com>
2021-06-28 10:08:27 +0200

    añadir enlace a página lista de tareas

--- assets/js/login.js
+++ assets/js/login.js
@@ -3,15 +3,21 @@ import {
   } from './llamadasApi.js';
         let email = document.getElementById('exampleInputEmail1').value;
         let password = document.getElementById('exampleInputPassword1').value;
-        data = {
+        let token;
+        let data = {
             email : email,
             password: password,
             _token : token
-
+        
         }
-       login(token, data = {})
+       login(email, password)
+       if(status == 200){
                     setTimeout(
                         function(){
                             window.location.href = "assets/registroAsistencia.html";
                         }, 3000);
-   
+                    }
+        else{
+            alert('Ha habido un problema')
+            console.log(login)
+        }
