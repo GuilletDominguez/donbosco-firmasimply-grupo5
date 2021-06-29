2aecfeac08ce5da3cf0e3a7e2fcdd49a7f935c48
Guillermo <you@example.com>
2021-06-29 09:09:17 +0200

    prueba

--- assets/js/firmasimply/login.js
+++ assets/js/firmasimply/login.js
@@ -1,12 +1,16 @@
 import Auth from './Modules/Auth/Auth.js';
 
 const DASHBOARD_URL = '/index.html';
-const CODER = {email: 'maria@firmasimply.com', password: 'password'};
 
 const btnLogin = document.querySelector('#login');
 btnLogin.addEventListener('click', login, false);
 
-async function login() {
+async function login(e) {
+  let email = document.getElementById('email').value;
+let password = document.getElementById('password').value;
+  const CODER = {email: email, password: password};
+
+  e.preventDefault()
   await Auth.login(CODER);
   console.log(CODER)
   window.location.assign(DASHBOARD_URL);
