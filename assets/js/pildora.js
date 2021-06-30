3696b5e9357f0b456d3857766e20f4e4f29dfc4a
mattthegoldenfox88 <82320244+mattthegoldenfox88@users.noreply.github.com>
2021-06-30 11:45:12 +0200

    cambio matias 13

--- assets/js/pildora.js
+++ assets/js/pildora.js
@@ -4,13 +4,21 @@ function crearpildora(){
 function eliminarPildora(){
     const elem = document.getElementById('lista');
     elem.style.backgroundColor='red';
-    console.log("kkade ekfjaselkjf");
     if (window.confirm("¿Estás seguro que deseas eliminar la píldora seleccionada?")) {
         let seleccionado = document.getElementById("selec");
         if (seleccionado.nodeValue == 'true'){
-            alert("Píldora eliminada");
+            alert("Píldora completada");
         }else{
-            alert("no ha pasado nada");
+            alert("No se han realizado cambios");
         }   
     }
 }
+var li = document.getElementsByClassName("list-group-item").length;
+console.log("Hay " + li + " li en total");
+
+function suma (){
+    let sum = li + 2;
+    document.querySelector(".pildorasTotales").innerHTML = sum;
+  };
+
+  suma();
