1c0109a2fa27e48570ac7287e938e3ff084e1f62
MohammadKa'ki <82204763+Mohammadkaki@users.noreply.github.com>
2021-07-08 14:23:02 +0200

    Update tareas.js

--- assets/js/firmasimply/tareas.js
+++ assets/js/firmasimply/tareas.js
@@ -1,37 +1,230 @@
-import Auth from './Modules/Auth/Auth.js';
-import Tarea from './Modules/Tarea.js';
+console.log("test");
+import Auth from './/Modules/Auth/Auth.js';
+import Tarea from './/Modules/Tarea.js';
 import Categoria from './Modules/Categoria.js';
 
 
-// Funcionalidad mostrar listado de tareas
-Tarea.getListadoTareas();
-// Funcionalidad crear una tarea
-let send= document.querySelector('#editsubmit')
-let titulo= document.querySelector('#nombre')
-let tarea = {
-    titulo: titulo,
-    descripcion: 'Lorem Ipsum',
-    estado: 0, // 0 pendiente, 1 completada
-    user_id: Auth.getCoder().id, // esta funcion devuelve el id del coder logeado
-    categoria_id: 1,
-};
-send.addEventListener('click', Tarea.crearTarea(tarea))
-Tarea.crearTarea(tarea)
+    
+// window.onload = function () {
 
-// Funcionalidad de borrar una tarea
-let idTarea = 2;
+//   var ctx = document.getElementById("barChart");
+//   var barChart = new Chart(ctx, {
+//       type: 'bar',
+//       data: {
+//           labels: ["Dog", "Cat", "Pangolin"],
+//           datasets: [{
+//               backgroundColor: '#00ff00',
+//               label: '# of Votes 2016',
+//               data: [12, 19, 3]
+//               }]
+//       }
+//   });
+  
+//   function addData(chart, label, color, data) {
+//       chart.data.datasets.push({
+//         label: label,
+//         backgroundColor: color,
+//         data: data
+//       });
+//       chart.update();
+//   }
+  
+//   // inserting the new dataset after 3 seconds
+//   setTimeout(function() {
+//     addData(barChart, '# of Votes 2017', '#ff0000', [16, 14, 8]);
+//   }, 3000);
+  
+//    }
 
-Tarea.borrarTarea(idTarea);
 
-// Marcar una tarea como completada o pendiente
-//let idTarea = 2;
-let data = { estado: 1 };          // 1 completada, 0 pendiente
-Tarea.marcarTarea(data, idTarea);
 
-// Funcionalidad de marcar una tarea como completada o pendiente
-let idPildora = 2;
-let data = { estado: 1 }; // 1 presentada, 0 pendiente
-Tarea.marcarPildora(data, idPildora);
 
-// Funcionalidad de cargar las categorias en el formulario de crear la tarea
-Categoria.getListadoCategorias();
+
+let infoCoder = Auth.getCoder()
+let emailUser = document.getElementById('usuario');
+ emailUser.innerHTML = infoCoder.name;
+
+ mostrarSeisTareas();
+
+ async function  mostrarSeisTareas(){
+     let listado = await Tarea.getListadoTareas();     
+     const list = document.getElementById('book-list'); 
+     // let ultimas = listado.slice(listado.length-6);
+     let ultimas = listado.reverse();
+     for (let x = 0; x < ultimas.length; x++){    
+         if (x < 6){   
+              
+             const row = document.createElement('tr');
+                
+             let text1;
+             if (listado[x].estado == 0){
+                 // console.log(listado[x].estado);
+                 text1 = `<td><label><input type="checkbox" id="${listado[x].id}" class="pulsado"  value="${listado[x].estado}">Pendiente</label></td>`;
+             }else{
+                 text1 = `<td><label><input type="checkbox" id="${listado[x].id}" class="pulsado" checked="true" value="${listado[x].estado}">Presentada</label></td>`;
+             }  
+
+             row.innerHTML = `
+                 <td>${listado[x].titulo}</td>
+                  <td>${listado[x].descripcion}</td>
+                 <td>${truncateString(listado[x].created_at,10)}</td>
+                 <td>${listado[x].id}</td>
+                 ${text1}
+                 <td><a href="#" id="${listado[x].id}" class="btn btn-danger btn-sm delete">X</td>
+                 ` 
+                 list.appendChild(row); 
+         }  
+     } 
+ 
+  
+  // Event: Add a Tarea
+  document.querySelector('#editsubmit').addEventListener('click', crearTarea, false )
+
+  async function crearTarea(e) {
+    e.preventDefault();
+    // Prevent actual submit
+    
+    let nombre = document.querySelector('#nombre').value;
+    let descripcion = document.querySelector('#descripcion').value;
+    let fecha = new Date();
+
+    let tarea = {
+      titulo: nombre,
+      descripcion: descripcion,                //'Lorem Ipsum',
+      fecha:fecha,
+      estado: 0,                              // 0 pendiente, 1 completada
+      user_id: Auth.getCoder().id,           // esta funcion devuelve el id del coder logeado
+      categoria_id: 1,
+     
+     };
+
+    // Validate
+    if(nombre  === '' || descripcion === '' || fecha === ''  ) {
+      showAlert('Por favor rellena todos los campos', 'danger');
+    } else {
+     
+      await Tarea.crearTarea(tarea);
+      // Add tarea
+      addtareaToList(tarea)  
+
+      // Clear fields
+      clearFields()
+
+    }
+  };
+
+ async function addtareaToList(tarea) {
+  const list = document.getElementById('book-list'); 
+    const row = document.createElement('tr');
+    row.innerHTML = `
+        <td>${tarea.titulo}</td>
+        <td>${tarea.descripcion}</td>
+        <td>${truncateString(tarea.fecha,10)}</td>
+        <td >${tarea.id}</td>
+        <td> <td><label><input type="checkbox" id="${tarea.id}" class="pulsado" > Presentada </label></td>
+        <td> <a href="#" id="${tarea.id}" class="btn btn-danger btn-sm delete"> X   </td>
+        ` 
+       list.appendChild(row); 
+  }
+
+  //<td><label><input type="checkbox" id="${tarea.id}" class="pulsado" value="${tarea.estado}">Presentada</label></td>
+  //<td> <button id="add" class="success "> &#10004; </button> </td>
+     
+  let o = document.querySelector('.pulsado');
+  o.style.fontSize = "100px";
+ 
+
+   function showAlert(message, className) {
+    const div = document.createElement('div');
+    div.className = `alert alert-${className}`;
+    div.appendChild(document.createTextNode(message));
+   
+   
+    const container = document.querySelector('.container');
+    const form = document.querySelector('#flexbox');
+    container.insertBefore(div, form); 
+
+   // Vanish in 3 seconds
+    setTimeout(() => document.querySelector('.alert').remove(), 3000);
+  }
+
+
+  function clearFields() {
+    document.querySelector('#nombre').value = '';
+ 
+    document.querySelector('#descripcion').value = '';
+  }
+
+
+  
+
+  //#MarcarTareas
+  document.querySelector('#book-list').addEventListener('click', marcarTareas ) 
+    async function marcarTareas(e) {
+     
+      let marcado = document.querySelector('.pulsado').checked;
+      
+      
+      if (marcado == true) {
+          let valor = e.target.value
+          valor = 1;
+          let data = {
+              estado : valor
+              
+          }
+          await Tarea.marcarTarea(data,e.target.id);
+          console.log(data);
+      }else
+      {   let valor = e.target.value
+          valor = 0;
+          let data = {
+              estado : valor
+          }
+          await Tarea.marcarTarea(data,e.target.id);
+          console.log(data);
+      }
+      
+      //window.location.reload(); 
+     
+    }
+ 
+
+  //#DeletTarea
+document.querySelector('#book-list').addEventListener('click', deletTarea);
+async function deletTarea(e){
+ 
+  if (e.target.classList.contains('delete')){
+   await Tarea.borrarTarea(e.target.id);
+    alert('Tarea Borrada');
+    window.location.reload();
+  
+  }
+}
+
+
+
+  // Event: Check
+  // document.querySelector('#book-list').addEventListener('click', (e) => {
+   
+  //   // check  target marks 
+  
+  //    console.log(e.target);
+ 
+  //   });
+     
+  
+
+        
+  Categoria.getListadoCategorias();
+  console.log(Categoria.getListadoCategorias);
+     
+function truncateString(str, num) {
+  if (str.length > num) {
+      let subStr = str.substring(0, num);
+      return subStr ;
+  } else {
+      return str;
+  }
+}
+
+}
